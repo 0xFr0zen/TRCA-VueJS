f05e6c47f5c61b11eb21b397f6f7c22409e65ff6
0xFr0zen <0xfr0zen@gmail.com>
2020-11-02 12:31:16 +0100

    some small fixes

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,73 @@
 <template>
   <div class="container">
-    <CBox
-      v-bind="mainStyles[colorMode]"
-      d="flex"
-      w="100vw"
-      h="100vh"
-      flex-dir="column"
-      justify-content="center"
-    >
-      <CHeading textAlign="center" mb="4">Twitch Redeem Command Alerts</CHeading>
-      <CFlex justify="center" direction="column" align="center">
-        
+    <CBox v-bind="mainStyles[colorMode]" d="flex" w="100vw" h="100vh" p="2" bg="gray.100">
+      <CFlex w="100vw" h="auto" flex-dir="column" p="2" align="center">
+        <CHeading textAlign="center" pb="2" color="indigo.500">Twitch Redeem Command Alerts</CHeading>
+        <CFlex justify="center" direction="column" p="4" w="50vw" h="auto" bg="gray.200" borderRadius="md">
+          <CBox d="flex" flex-dir="column">
+            <CBox d="flex" w="100%">
+              <CText size="sm" color="indigo.500" p="2" fontWeight="500">Load Commands</CText>
+              <CIconButton @click="loadCommands" icon="search" />
+            </CBox>
+            <CBox>
+              <CHeading size="lg" d="flex" pb="2" color="indigo.500">Commands</CHeading>
+              <CBox>
+                <CStack spacing="5" is-inline mb="3">
+                  <CBox
+                    bg="indigo.300"
+                    borderRadius="md"
+                    v-for="command in commands"
+                    v-bind:key="command.id"
+                    p="4"
+                    border-width="1px"
+                    cursor="pointer"
+                  >
+                    <CHeading size="lg" color="white">!{{ command.name }}</CHeading>
+                    <CText mt="4" color="white">{{ command.text }}</CText>
+                    <CImage mt="4"></CImage>
+                  </CBox>
+                </CStack>
+                <CBox d="flex" flex-dir="row">
+                  Hello
+                </CBox>
+              </CBox>
+            </CBox>
+          </CBox>
+        </CFlex>
       </CFlex>
     </CBox>
   </div>
 </template>
 
 <script>
-import {
-  CBox,
-  CFlex,
-  CHeading
-} from "@chakra-ui/vue";
+import { CBox, CFlex, CHeading, CStack, CText, CIconButton } from '@chakra-ui/vue';
+import axios from 'axios';
 
 export default {
-  name: "App",
-  inject: ["$chakraColorMode", "$toggleColorMode"],
+  name: 'App',
+  inject: ['$chakraColorMode', '$toggleColorMode'],
   components: {
     CBox,
     CFlex,
-    CHeading
+    CHeading,
+    CStack,
+    CText,
+    CIconButton
   },
   data() {
     return {
       showModal: false,
       mainStyles: {
         dark: {
-          bg: "gray.700",
-          color: "whiteAlpha.900"
+          bg: 'gray.700',
+          color: 'whiteAlpha.900'
         },
         light: {
-          bg: "white",
-          color: "gray.900"
+          bg: 'white',
+          color: 'gray.900'
         }
-      }
+      },
+      commands: [{}]
     };
   },
   computed: {
@@ -58,14 +82,22 @@ export default {
     }
   },
   methods: {
-    showToast() {
+    showToast(title, description) {
       this.$toast({
-        title: "Account created.",
-        description: "We've created your account for you.",
-        status: "success",
+        title,
+        description,
+        status: 'info',
         duration: 10000,
-        isClosable: true
+        isClosable: true,
+        position: 'bottom-right',
+        variant: 'solid'
       });
+    },
+    async loadCommands() {
+      axios
+        .get('http://localhost:3000/u/oetziofficial/cs')
+        .then(cs => (this.commands = cs.data))
+        .catch(e => console.error(e));
     }
   }
 };
